759559f41d4c4d85172287b3ca87ac6c3349e5af
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-05-21 10:15:13 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,27 @@
+require "pry"
+class Post
+  attr_accessor :author, :title
+
+@@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if author
+    self.author.name
+  else nil
+    # binding.pry
+
+end
+  end
+
+
+
+end
